31357cf9240ecd269573d83c29e062c5f506cd74
Leon Dumestre <leon.dumestre@gmail.com>
2023-12-05 17:07:03 +0100

    chore: init simulation web interface

---
+++ simulation/web-interface/app.vue
@@ -0,0 +1,29 @@
+<template>
+  <main>
+    <l-map ref="map" v-model:zoom="zoom" v-model:center="center" :useGlobalLeaflet="false">
+      <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"></l-tile-layer>
+    </l-map>
+  </main>
+</template>
+
+<script lang="ts" setup>
+import "leaflet/dist/leaflet.css"
+import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet"
+
+useHead({ title: 'Simulation Interface' })
+
+let zoom = ref(13)
+let center = ref([45.750, 4.856])
+</script>
+
+<style lang="scss" scoped>
+html, body {
+  margin: 0;
+  padding: 0;
+}
+
+main {
+  height: 100vh;
+  width: 100vw;
+}
+</style>
