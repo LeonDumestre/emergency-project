01f337536d302ccbe4b96f9b11b56ba18deef087
Leon Dumestre <leon.dumestre@gmail.com>
2023-12-06 12:11:05 +0100

    feat: add view without nuxt

--- simulator/simulator-view-nuxt/nuxt.config.ts
+++ simulator/simulator-view-nuxt/nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  devServer: {
+    port: 3020,
+  },
   devtools: { enabled: true },
   css: ['leaflet/dist/leaflet.css'],
 })
---
+++ simulator/simulator-view/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <main>
+    <div id="map" style="height: 100vh" />
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+export default defineComponent({
+  name: "App",
+  mounted() {
+    // Initialisez la carte Leaflet
+    const map = L.map("map").setView([45.75, 4.856], 13);
+
+    // Ajoutez une couche de carte (par exemple, OpenStreetMap)
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: "&copy; OpenStreetMap contributors",
+    }).addTo(map);
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+main {
+  margin: 0;
+  padding: 0;
+}
+
+#map {
+  height: 100vh;
+  width: 100vw;
+}
+</style>
