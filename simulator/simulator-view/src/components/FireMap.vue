9d3a823ed3f21bc8bdd27aa7de99d6a378ccc4b0
Leon Dumestre <leon.dumestre@gmail.com>
2023-12-20 12:03:18 +0100

    refactor(simulator-view): move map functions in utils file

--- simulator/simulator-view/src/components/FireMap.vue
+++ simulator/simulator-view/src/components/FireMap.vue
@@ -5,49 +5,23 @@
 <script lang="ts">
 import L from "leaflet";
 import { defineComponent } from "vue";
-import { Fire } from "@/utils/fire.model";
-import { formatDateWithHour } from "@/utils/date.utils";
+import { initMap, addCircle } from "@/utils/map.utils";
 import { getFires } from "@/utils/fire.request";
 
 export default defineComponent({
   name: "App",
+
   async mounted() {
     const map = initMap();
 
     const fires = await getFires();
-    fires.map((fire) => addMarker(map, fire));
+    fires.map((fire) => addCircle(map, fire));
   },
+
   beforeUnmount() {
     L.map("map").remove();
   },
 });
-
-function initMap(): L.Map {
-  // Initialisez la carte Leaflet
-  const minZoom = 13;
-  const map = L.map("map", { minZoom }).setView([45.75, 4.856], 13);
-
-  // Ajoutez une couche de carte OpenStreetMap
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: "&copy; OpenStreetMap contributors",
-  }).addTo(map);
-
-  return map;
-}
-
-function addMarker(map: L.Map, fire: Fire) {
-  const circle = L.circle([fire.latitude, fire.longitude], {
-    radius: fire.intensity * 50,
-    color: "red",
-    fillOpacity: 0.2,
-  }).addTo(map);
-
-  const triggerAt = formatDateWithHour(fire.triggerAt);
-  circle.bindPopup(
-    `Intensité: ${fire.intensity}<br />
-    Déclenchement: ${triggerAt}`
-  );
-}
 </script>
 
 <style lang="scss" scoped>
