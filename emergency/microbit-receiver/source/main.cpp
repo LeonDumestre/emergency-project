63f45dd9dd3a1a14e1533513b360f690e9074ce9
louischarnay <louis.charnay@orange.fr>
2023-12-21 17:11:40 +0100

    feat(microbit): update microbit code

--- emergency/microbit-receiver/source/main.cpp
+++ emergency/microbit-receiver/source/main.cpp
@@ -1,28 +1,108 @@
+// #include "MicroBit.h"
+// #include "crypt.hpp"
+// #include <string>
+
+// MicroBit uBit;
+
+// const uint8_t key[16] = { 0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c };
+
+// ManagedString managed_string;
+
+// void onRadioReceive(MicroBitEvent) {
+//     managed_string = uBit.radio.datagram.recv();
+//     uBit.serial.printf("%s~", decrypt((char*)managed_string.toCharArray(), (uint8_t*)key));
+// }
+
+// int main() {
+//     uBit.init();
+
+//     uBit.messageBus.listen(MICROBIT_ID_RADIO, MICROBIT_RADIO_EVT_DATAGRAM, onRadioReceive);
+    
+//     uBit.radio.enable();
+//     uBit.radio.setGroup(28);
+
+//     while(true) {
+//         uBit.sleep(8000);
+//     }
+
+//     release_fiber();
+// }
+
+
 #include "MicroBit.h"
-#include "crypt.hpp"
-#include <string>
+#include "aes.hpp"
 
 MicroBit uBit;
+uint8_t key[16] = "Jeremy";
 
-const uint8_t key[16] = { 0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c };
-
-ManagedString managed_string;
-
-void onRadioReceive(MicroBitEvent) {
-    managed_string = uBit.radio.datagram.recv();
-    uBit.serial.printf("%s~", decrypt((char*)managed_string.toCharArray(), (uint8_t*)key));
+/**
+ * Déchiffre le texte passé en paramètre (le texte doit être d'une taille de 240 octets)
+ */
+void decrypt(uint8_t *encrypted_text)
+{
+    struct AES_ctx ctx;
+    AES_init_ctx(&ctx, key);
+    for (int i = 0; i < 15; ++i)
+    {
+        AES_ECB_decrypt(&ctx, encrypted_text + (i * 16));
+    }
 }
 
-int main() {
+/**
+ * Fonction appelé lors de la réception de detagram via radio.
+ */
+void onRadioReceive(MicroBitEvent)
+{
+    // Réception du datagram
+    PacketBuffer pb = uBit.radio.datagram.recv();
+
+    // Déclaration de la chaine de charactère et copie des informations reçus.
+    uint8_t *myjson = (uint8_t *)malloc(240 * sizeof(uint8_t));
+    memcpy(myjson, pb.getBytes(), 240);
+
+    // Déchiffrement du texte
+    decrypt(myjson);
+
+    // Envoi du texte déchiffré en serial à la passerelle.
+    uBit.serial.send(((char *)myjson));
+    free(myjson);
+}
+
+int main()
+{
+    // Initialise the micro:bit runtime.
     uBit.init();
-
     uBit.messageBus.listen(MICROBIT_ID_RADIO, MICROBIT_RADIO_EVT_DATAGRAM, onRadioReceive);
-    
     uBit.radio.enable();
-    uBit.radio.setGroup(28);
+    uBit.radio.setGroup(83);
+    uBit.serial.setRxBufferSize(240);
 
-    while(true) {
-        uBit.sleep(8000);
+    // Initialisation de l'outil de chiffrement et du buffer utilisé pour la réception de message via UART.
+    struct AES_ctx ctx;
+    AES_init_ctx(&ctx, key);
+    uint8_t data[240] = "";
+    memset(data, 0, sizeof(data));
+
+    while (true)
+    {
+        if (uBit.serial.rxBufferedSize() > 0)
+        {
+            // Réception du message passé via UART et copie dans une châine de charactère.
+            ManagedString s = uBit.serial.readUntil(";");
+            strcat((char *)data, (char *)s.toCharArray());
+
+            // Chiffre puis envoie le code reçu
+            for (int i = 0; i < 15; ++i)
+            {
+                AES_ECB_encrypt(&ctx, data + (i * 16));
+            }
+            uBit.radio.datagram.send((char *)data);
+
+            // Réinitialisation du buffer
+            memset(data, 0, sizeof(data));
+        }
+
+        uBit.sleep(50);
     }
 
     release_fiber();
--- simulator/microbit-sender/source/main.cpp
+++ simulator/microbit-sender/source/main.cpp
@@ -1,39 +1,84 @@
+// #include "MicroBit.h"
+// #include "crypt.hpp"
+// #include <string>
+
+// MicroBit uBit;
+
+// const uint8_t key[16] = { 0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c };
+// const int ONE_SECOND_IN_MS = 1000;
+
+// string serial_msg;
+
+// void transit(const char* input){
+//     uBit.radio.datagram.send(encrypt((char*)input, (uint8_t*)key));
+// }
+
+// void onSerialReceive(MicroBitEvent) {
+//     serial_msg = uBit.serial.readUntil("~").toCharArray();
+//     serial_msg.erase(remove(serial_msg.begin(), serial_msg.end(), '~'), serial_msg.end());
+//     transit(serial_msg.c_str());
+//     uBit.serial.printf("#");
+// }
+
+
+// int main() {
+//     uBit.init();
+
+//     uBit.serial.setRxBufferSize(255);
+//     uBit.serial.eventOn("~");
+
+//     uBit.messageBus.listen(MICROBIT_ID_SERIAL, MICROBIT_SERIAL_EVT_DELIM_MATCH, onSerialReceive);
+    
+//     uBit.radio.enable();
+//     uBit.radio.setGroup(28);
+
+//     while(true) {
+//         uBit.sleep(1 * ONE_SECOND_IN_MS);
+//     }
+
+//     release_fiber();
+// }
+
 #include "MicroBit.h"
-#include "crypt.hpp"
-#include <string>
+#include "aes.hpp"
 
 MicroBit uBit;
+uint8_t key[16] = "Jeremy";
 
-const uint8_t key[16] = { 0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c };
-const int ONE_SECOND_IN_MS = 1000;
-
-string serial_msg;
-
-void transit(const char* input){
-    uBit.radio.datagram.send(encrypt((char*)input, (uint8_t*)key));
-}
-
-void onSerialReceive(MicroBitEvent) {
-    serial_msg = uBit.serial.readUntil("~").toCharArray();
-    serial_msg.erase(remove(serial_msg.begin(), serial_msg.end(), '~'), serial_msg.end());
-    transit(serial_msg.c_str());
-    uBit.serial.printf("#");
-}
-
-
-int main() {
+int main()
+{
+    // Initialise the micro:bit runtime.
     uBit.init();
-
-    uBit.serial.setRxBufferSize(255);
-    uBit.serial.eventOn("~");
-
-    uBit.messageBus.listen(MICROBIT_ID_SERIAL, MICROBIT_SERIAL_EVT_DELIM_MATCH, onSerialReceive);
-    
     uBit.radio.enable();
-    uBit.radio.setGroup(28);
+    uBit.radio.setGroup(80);
+    uBit.serial.setRxBufferSize(240);
 
-    while(true) {
-        uBit.sleep(1 * ONE_SECOND_IN_MS);
+    // Initialisation de l'outil de chiffrement et du buffer utilisé pour la réception de message via UART.
+    struct AES_ctx ctx;
+    AES_init_ctx(&ctx, key);
+    uint8_t data[240] = "";
+    memset(data, 0, sizeof(data));
+
+    while (true)
+    {
+        if (uBit.serial.rxBufferedSize() > 0)
+        {
+            // Réception du message passé via UART et copie dans une châine de charactère.
+            ManagedString s = uBit.serial.readUntil(";");
+            strcat((char *)data, (char *)s.toCharArray());
+
+            // Chiffre puis envoie le code reçu
+            for (int i = 0; i < 15; ++i)
+            {
+                AES_ECB_encrypt(&ctx, data + (i * 16));
+            }
+            uBit.radio.datagram.send((char *)data);
+
+            // Réinitialisation du buffer
+            memset(data, 0, sizeof(data));
+        }
+
+        uBit.sleep(50);
     }
 
     release_fiber();
