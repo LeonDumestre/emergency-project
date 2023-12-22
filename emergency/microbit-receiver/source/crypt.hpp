4be7167dd376b868df0da03590d6ee2e77cb65e1
Leon Dumestre <leon.dumestre@cpe.fr>
2023-12-22 16:30:44 +0100

    fix(microbit): remove unused files

--- emergency/microbit-receiver/source/crypt.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "aes.hpp"
-
-char* encrypt(char* msg, uint8_t* key);
-
-//Hugo
-char* decrypt(char* hash, uint8_t* key);
-
--- emergency/microbit-receiver/source/main.cpp
+++ emergency/microbit-receiver/source/main.cpp
@@ -1,30 +1,3 @@
-// #include "MicroBit.h"
-// #include "crypt.hpp"
-// #include <string>
-
-// MicroBit uBit;
-
-// const uint8_t key[16] = { 0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c };
-
-// ManagedString managed_string;
-
-// void onRadioReceive(MicroBitEvent) {
-//     managed_string = uBit.radio.datagram.recv();
-//     uBit.serial.printf("%s;", decrypt((char*)managed_string.toCharArray(), (uint8_t*)key));
-// }
-
-// int main() {
-//     uBit.init();
-
-//     uBit.messageBus.listen(MICROBIT_ID_RADIO, MICROBIT_RADIO_EVT_DATAGRAM, onRadioReceive);
-    
-//     uBit.radio.enable();
-//     uBit.radio.setGroup(28);
-
-//     release_fiber();
-// }
-
-
 #include "MicroBit.h"
 #include "aes.hpp"
 
