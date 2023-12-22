6cd5a75c51f4505317291c29d479499e534829b2
Leon Dumestre <leon.dumestre@gmail.com>
2023-12-22 13:56:26 +0100

    fix: cors

--- emergency/emergency-web-server/src/main.ts
+++ emergency/emergency-web-server/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 
-const allowedOrigins = ["http://localhost:3020"];
+const allowedOrigins = ["http://localhost:3010", "http://localhost:3020"];
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
