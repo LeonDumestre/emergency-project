a0bc9c4e2a3be78f21eee03025b6781521a27e0b
Dorian Gorse <doriancj.gorse@gmail.com>
2024-01-15 13:11:04 +0100

    feat: new data to mqtt

--- emergency/influx/subscriber.py
+++ emergency/influx/subscriber.py
@@ -20,6 +20,7 @@ write_api = client.write_api()
 # MQTT broker config
 MQTT_BROKER_URL    = "mqtt.eclipseprojects.io"
 MQTT_PUBLISH_TOPIC = "sensors"
+MQTT_PUBLISH_TOPIC = "fires"
 
 mqttc = mqtt.Client()
 mqttc.connect(MQTT_BROKER_URL)
@@ -40,17 +41,31 @@ def on_message(client, userdata, msg):
         ready = str(msg.payload.decode('utf-8')).replace("'", "\"")
         data = json.loads(ready)
         
-        # Extract ID and temperature from the payload
-        sensor_id = data.get("id")
-        number_of_fire = data.get("length")
+        if(data.get("type") == "sensor"):
+            # Extract ID and temperature from the payload
+            sensor_id = data.get("id")
+            number_of_fire = data.get("length")
 
-        if sensor_id is not None and number_of_fire is not None:
-            ## InfluxDB logic
-            point = Point(MQTT_PUBLISH_TOPIC).tag("id", sensor_id).field("fire", number_of_fire)
-            write_api.write(bucket=BUCKET, record=point)
-            print(f"Data written to InfluxDB - ID: {sensor_id}, Number of fire: {number_of_fire}")
-        else:
-            print("Invalid payload format. Missing 'id' or 'temperature'.")
+            if sensor_id is not None and number_of_fire is not None:
+                ## InfluxDB logic
+                point = Point(MQTT_PUBLISH_TOPIC).tag("id", sensor_id).field("fire", number_of_fire)
+                write_api.write(bucket=BUCKET, record=point)
+                print(f"Data written to InfluxDB - ID: {sensor_id}, Number of fire: {number_of_fire}")
+            else:
+                print("Invalid payload format. Missing 'id' or 'temperature'.")
+        elif(data.get("type") == "fire"):
+            # Extract ID and temperature from the payload
+            fire_id = data.get("id")
+            intensity = data.get("intensity")
+            distance = data.get("distance")
+
+            if fire_id is not None and intensity is not None and distance is not None:
+                ## InfluxDB logic
+                point = Point(MQTT_PUBLISH_TOPIC).tag("id", fire_id).field("intensity", intensity).field("distance", distance)
+                write_api.write(bucket=BUCKET, record=point)
+                print(f"Data written to InfluxDB - ID: {fire_id}, Intensity: {intensity}, Distance: {distance}")
+            else:
+                print("Invalid payload format. Missing 'id' or 'temperature'.")
     except json.JSONDecodeError as e:
         print(f"Error decoding JSON: {e}")
         
